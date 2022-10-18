5cc48375f2bd0a39905c7ec38b56ba38069e3638
NokibulAlam <nokibul.alam02@gmail.com>
2022-10-18 19:02:44 +0600

    Card Added in Proccessing Section

--- src/components/Core/Processing.js
+++ src/components/Core/Processing.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import Card from '../extra/Card';
+
+// Images
+import CT from '../../images/Processing/Collecting.jpg';
+import PR from '../../images/Processing/Proccessing.jpg';
+import TT from '../../images/Processing/Testing.jpg';
+import EX from '../../images/Processing/Export.jpg';
 
 const Processing = () => {
     return (
         <div name="processing">
             <Container>
+
                 <Row>
                     <Col md={12} sm={12}>
                         <div>
-                            <p className=' text-[#7a462a] text-3xl sm:text-5xl font-semibold text-center mt-14 uppercase'>processing</p>
+                            <p className=' text-[#7a462a] text-3xl sm:text-5xl text-center mt-14 uppercase'>processing</p>
                         </div>
                     </Col>
                 </Row>
+
+                <Row>
+                    <Col md={12} sm={12}>
+                        <div>
+                            <p className=' text-[#2b1c14] text-center mt-10 lg:mx-14'>We start from raw hides and end with identical quality
+                                finished leather.
+                                During this transformation, the skin passes through various steps of the latest technologies.
+                                We love the raw skin & preserve in modern refrigerators to maintain better quality Constant
+                                research & new developments is an integral part of SAF policy.</p>
+                        </div>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col md={3} sm={12}>
+                        <div className='flex gap-x-14 text-center'>
+                            <Card image={CT} title="Collect Skin" />
+                        </div>
+                    </Col>
+                    <Col md={3} sm={12}>
+                        <div className='flex gap-x-14 text-center'>
+                            <Card image={PR} title="Proccessing" />
+                        </div>
+                    </Col>
+                    <Col md={3} sm={12}>
+                        <div className='flex gap-x-14 text-center'>
+                            <Card image={TT} title="Tesing" />
+                        </div>
+                    </Col>
+                    <Col md={3} sm={12}>
+                        <div className='flex gap-x-14 text-center'>
+                            <Card image={EX} title="Export" />
+                        </div>
+                    </Col>
+                </Row>
+
             </Container>
         </div>
     )
---
+++ src/components/extra/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = (props) => {
+    return (
+        <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
+            <img class="rounded-t-lg" src={props.image} alt="" />
+
+            <div class="p-5">
+                <h5 class="mb-2  font-semibold tracking-tight text-[#7a462a] ">{props.title}</h5>
+                <p class="mb-3 font-normal text-gray-400 ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
